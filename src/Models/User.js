14d456a7eb7df3f46e3eec850af061377abb43f5
DEVID19 <devidbisen84@gmail.com>
2025-11-15 12:40:29 +0530

    data sanitization done

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,24 +1,62 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender must be male, female, or other");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+      lowercase: true,
+      trim: true,
+    },
+    photoURL: {
+      type: String,
+      trim: true,
+      default: "https://example.com/default-photo.jpg",
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    githubURL: {
+      type: String,
+      trim: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
--- src/app.js
+++ src/app.js
@@ -25,7 +25,88 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("data Save Successfully");
   } catch (error) {
-    res.status(400).send("Error in saving the data to database");
+    res.status(400).send("Error : " + error.message);
+  }
+});
+//  Get user data by using the emailid
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    console.log("userEmail is :", userEmail);
+    const users = await User.find({ emailId: userEmail });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error in finding the user");
+  }
+});
+
+// get all data in the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error in finding the entire data in the database");
+  }
+});
+
+//* delete the documents in the database usingb the email id and the userid
+//? deleted using the emailid
+// app.delete("/delete", async (req, res) => {
+//   try {
+//     const userEmail = req.body.emailId;
+//     const users = await User.deleteOne({ emailId: userEmail });
+//     res.send("data is deleted successfully using the emailid");
+//   } catch (error) {
+//     res.status(400).send("Unable to delete the database ");
+//   }
+// });
+//! deleted using the userid
+
+app.delete("/delete", async (req, res) => {
+  try {
+    const userId = req.body._id;
+    const users = await User.deleteOne({ _id: userId });
+    res.send("data successfully deleted using the _id ");
+  } catch (error) {
+    res.status(400).send("Unable to delete the database");
+  }
+});
+
+//* update the document using the userid
+
+app.patch("/update", async (req, res) => {
+  try {
+    const userId = req.body._id;
+
+    // Check if _id is provided
+    if (!userId) {
+      return res.status(400).send("User ID (_id) is required in request body");
+    }
+
+    // Remove _id from the update data
+    const { _id, ...updatedData } = req.body;
+
+    // Check if there's any data to update
+    if (Object.keys(updatedData).length === 0) {
+      return res.status(400).send("No fields to update");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send("Successfully updated the data");
+  } catch (error) {
+    console.error("Update Error:", error.message);
+    res.status(400).send("Unable to update the database: " + error.message);
   }
 });
 
